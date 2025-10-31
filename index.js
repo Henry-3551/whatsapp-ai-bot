1a078276b0537e5c31b86da77d409a98297ed6dc
Ani Victory-Henry Chukwuemerie <104362482+Henry-3551@users.noreply.github.com>
2025-10-31 07:07:10 +0100

    Add environment variable declarations

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
   })
 );
 
+// env variables
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
 const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
